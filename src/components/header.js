50ade357896ac35a18ce1f8945dca3909001a614
womedizioni <hypnos.design@gmail.com>
2020-02-16 00:56:04 +0100

    aggiunto stile 7in1

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,19 @@
 import React from "react";
 import {Link} from "gatsby";
+import headerStyles from "./header.module.sass"
 
 const Header = () => {
   return (
-    <header>
+    <header className={headerStyles}>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/lacasaeditrice">Casa Editrice</Link></li>
-          <li><Link to="/catalogo">Catalogo</Link></li>
-          <li><Link to="/lagenda">Agenda</Link></li>
-          <li><Link to="laperiodico">l'aperiodico</Link></li>
-          <li><Link to="labottega">la bottega</Link></li>
-          <li><Link to="contatti">contatti</Link></li>
+          <li><Link to="/" activeClassName={headerStyles.active}>Home</Link></li>
+          <li><Link to="/lacasaeditrice" activeClassName={headerStyles.active}>Casa Editrice</Link></li>
+          <li><Link to="/catalogo" activeClassName={headerStyles.active}>Catalogo</Link></li>
+          <li><Link to="/lagenda" activeClassName={headerStyles.active}>Agenda</Link></li>
+          <li><Link to="laperiodico" activeClassName={headerStyles.active}>l'aperiodico</Link></li>
+          <li><Link to="labottega" activeClassName={headerStyles.active}>la bottega</Link></li>
+          <li><Link to="contatti" activeClassName={headerStyles.active}>contatti</Link></li>
         </ul>
       </nav>
     </header>
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import "../styles/main.sass"
+import LayoutStyles from "./layout.module.sass";
 
 const Layout = (props) => {
   return (
-    <div>
-      <Header/>
-      {props.children}
+    <div className={LayoutStyles.container}>
+      <div className={LayoutStyles.content}>
+        <Header/>
+        {props.children}
+      </div>
       <Footer/>
     </div>
   )

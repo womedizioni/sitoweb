be73c578b53c1388fb4f75c679c7da828b690626
womedizioni <hypnos.design@gmail.com>
2020-02-15 20:29:33 +0100

    aggiunte pagine

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header/header";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>Home di Wom Edizioni</h1>
+    </div>
+  )
+};
+
+export default IndexPage
